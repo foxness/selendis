1563f6a1f5b96e9d00c976a55184e3655a7304f8
Rivershy <foxnezz@gmail.com>
2020-09-27 22:39:54 +0200

    Initial Commit

---
+++ Selendis/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Selendis
+//
+//  Created by user179838 on 9/27/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
